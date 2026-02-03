6a4f56e58084acfec96bfa4d918c22eca4b48ef6
אביאל <x0583289789@gmail.com>
2026-02-03 11:27:55 +0200

    Update php.py

--- php.py
+++ php.py
@@ -1,5 +1,7 @@
 import flask as fl
 import requests as re
-app = Flask(__main__)
-@app.route('\',methods=['GET'])
+app = fl.Flask(__name__)
+@app.route('/',methods=['GET'])
 def a():return re.get('view-source:' + fl.request.args['x'] if fl.request.args['x'] else 'https://aviel1.vercel.app')
+if __name__ == '__main__':
+    app.run(debug=True)
