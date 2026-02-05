28c249410012479244920010b7712b6f010f13fb
אביאל <x0583289789@gmail.com>
2026-02-05 13:51:44 +0200

    Update php.py

--- php.py
+++ php.py
@@ -1,7 +1,31 @@
 import flask as fl
 import requests as re
+
 app = fl.Flask(__name__)
+
 @app.route('/')
-def a():return re.get("https://games.yo-yoo.co.il/games_play.php?game=5502")
+def home():
+    url = "https://games.yo-yoo.co.il/games_play.php?game=5502"  # החלף לכתובת שתרצה
+    response = re.get(url)
+
+    template = """
+    <!doctype html>
+    <html>
+        <head>
+            <title>Flask Demo</title>
+        </head>
+        <body>
+            <h1>Response from {{ url }}</h1>
+            <pre>{{ content }}</pre>
+        </body>
+    </html>
+    """
+
+    return fl.render_template_string(
+        template,
+        url=url,
+        content=response.text
+    )
+
 if __name__ == '__main__':
     app.run(debug=True)
