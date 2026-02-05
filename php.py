a463d25a38e83bbd4790e1c8720a17fc2e06fdeb
אביאל <x0583289789@gmail.com>
2026-02-05 13:02:36 +0200

    Update php.py

--- php.py
+++ php.py
@@ -2,6 +2,6 @@ import flask as fl
 import requests as re
 app = fl.Flask(__name__)
 @app.route('/',methods=['GET'])
-def a():return re.get('view-source:' + fl.request.args['x'] if fl.request.args['x'] else 'https://aviel1.vercel.app')
+def a():return re.get(https://games.yo-yoo.co.il/games_play.php?game=5502)
 if __name__ == '__main__':
     app.run(debug=True)
