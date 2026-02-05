729476680bfda72412a248c7ec9742cc252836b9
אביאל <x0583289789@gmail.com>
2026-02-05 13:44:44 +0200

    Update php.py

--- php.py
+++ php.py
@@ -1,7 +1,7 @@
 import flask as fl
 import requests as re
 app = fl.Flask(__name__)
-@app.route('/',methods=['GET'])
+@app.route('/')
 def a():return re.get("https://games.yo-yoo.co.il/games_play.php?game=5502")
 if __name__ == '__main__':
     app.run(debug=True)
